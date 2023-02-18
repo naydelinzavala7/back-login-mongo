8bb7e7c8325d1a67426d197d86f87c3546225657
naydelinzavala7 <alondrazavala503@gmail.com>
2023-02-18 15:19:28 -0600

    VOLVIMOS A CONFIGURAR HASTA EL UPDATE

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const Joi = require('@hapi/joi')
 const bcrypt = require('bcrypt')
 const Jwt = require('jsonwebtoken')
 
+//------------------------------------------
 const validacionregistro = Joi.object({
     name: Joi.string().max(255).required(),
     apaterno: Joi.string().max(255).required(),
@@ -11,12 +12,22 @@ const validacionregistro = Joi.object({
     email: Joi.string().max(255).required(),
     password: Joi.string().min(6).max(1024).required()
 })
-
+//-------------------------------------------
 const validacionLogin = Joi.object({
     email: Joi.string().max(255).required(),
     password: Joi.string().min(6).max(1024).required()
 })
+//------------------------------------------
+const validacionUpdate = Joi.object({
+    id: Joi.string().max(1024).required(),
+    name: Joi.string().max(255).required(),
+    apaterno: Joi.string().max(255).required(),
+    amaterno: Joi.string().max(255).required(),
+    password: Joi.string().min(6).max(1024).required()
+})
 
+
+//-------------------------------------------- REGISTRAR
 router.post('/register', async(req,res) => {
     const { error } = validacionregistro.validate(req.body)
 
@@ -65,7 +76,8 @@ router.post('/register', async(req,res) => {
     }
 })
 
-router.post('/login', async(req,res) => {
+//--------------------------------------------- LOGIN
+router.post('/login', async(req, res) => {
     const { error } = validacionLogin.validate(req.body)
 
     if ( error ) {
@@ -89,7 +101,7 @@ router.post('/login', async(req,res) => {
     if ( !passwordCorrecto ){
         return res.status(400).json({
             error: "Las contraseñas no coinciden"
-        })
+        })  
     }
 
 
@@ -102,6 +114,92 @@ router.post('/login', async(req,res) => {
         error: null,
         data: { token }
     })
+})
+
+//--------------------------------------------- OBTENER TODOS LOS USUARIOS DE LA BASE DE DATOS
+router.get('/getallusers', async(req,res) => {
+    const users = await User.find()
+
+    if (users) {
+        res.json({
+            error: null,
+            data: users
+        })
+    } else {
+        res.json({
+            error: "No hay usuarios en la BD"
+        })
+    }
+})
+
+//--------------------------------------------- BORRAR UN USUARIO ESPECIFICO DE LA BASE DE DATOS
+router.post('/deleteuser', async(req, res) =>{
+    const id = req.body.id 
+    const userExist = await User.findById({_id: id})
+    if(userExist){
+        await User.findByIdAndDelete({_id: id})
+        res.json({
+            error: null,
+            data: 'Usuario Eliminado de la BD'
+        })
+    } else {
+        res.json({
+            error: 'Usuario no encontrado'
+        })
+    }
+})
+
+//--------------------------------------------- MODIFICAR MENOS CORREO
+router.post('/updateuser', async(req,res) => {
+    const { error } = validacionUpdate.validate(req.body)
+
+    if ( error ) {
+        return res.status(400).json({
+            error: error.details[0].message
+        })
+    }
+
+   let existeId = await User.findOne({
+        _id: req.body.id
+    })
+
+    if ( !existeId ){
+        return res.status(400).json({
+            error: "El usuario no existe"
+        })
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const contrasenaNueva = await bcrypt.hash(req.body.password, salt)
+    //existeId.password = contrasenaNueva
+    existeId = {
+        id: req.body.id,
+        name: req.body.name,
+        apaterno: req.body.apaterno,
+        amaterno: req.body.amaterno,
+        password: contrasenaNueva
+    }
+    try{
+        const guardado = await User.findByIdAndUpdate(
+            {_id: existeId.id},
+            existeId,
+            { new: true }
+        )
+        if( guardado ) {
+            return res.json({
+                error: null,
+                data: guardado
+            })
+        } else {
+            return res.json({
+                error: "No se pudo actualizar:(" 
+            })
+        }
+    } catch (error) {
+        return res.json({
+            error
+        })
+    }
 })
 
 module.exports = router
